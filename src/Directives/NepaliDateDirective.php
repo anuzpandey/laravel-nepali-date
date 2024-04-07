2a2dbe2a3d0afd59cc5e9e6c21bfcb6bd6345f5a
AnuzPandey <anuzbvbmaniac123@gmail.com>
2024-04-07 23:25:11 +0545

    :hammer: fix: blade directives signature.

--- src/Directives/NepaliDateDirective.php
+++ src/Directives/NepaliDateDirective.php
@@ -8,14 +8,30 @@ class NepaliDateDirective
 {
     public static function register(): void
     {
-        Blade::directive('nepaliDate', function ($date = null, $format = null, $locale = null) {
-            $date ??= now();
-            return "<?php echo Anuzpandey\LaravelNepaliDate\LaravelNepaliDate::from($date)->toNepaliDate($format, $locale) ?>";
+        Blade::directive('nepaliDate', function (string $expression) {
+            [$date, $format, $locale] = self::getParsedArguments($expression);
+
+            return "<?php echo Anuzpandey\LaravelNepaliDate\LaravelNepaliDate::from({$date})->toNepaliDate({$format}, {$locale}) ?>";
         });
 
-        Blade::directive('englishDate', function ($date = null, $format = null, $locale = null) {
-            $date ??= now();
-            return "<?php echo Anuzpandey\LaravelNepaliDate\LaravelNepaliDate::from($date)->toEnglishDate($format, $locale) ?>";
+        Blade::directive('englishDate', function (string $expression) {
+            [$date, $format, $locale] = self::getParsedArguments($expression);
+
+            return "<?php echo Anuzpandey\LaravelNepaliDate\LaravelNepaliDate::from({$date})->toEnglishDate({$format}, {$locale}) ?>";
         });
+    }
+
+    private static function getParsedArguments(string $expression): array
+    {
+        $segments = explode(',', str_replace(['(', ')', ' '], '', $expression));
+        $date = $segments[0] ?? null;
+        $format = $segments[1] ?? null;
+        $locale = $segments[2] ?? null;
+
+        return [
+            $date === 'now' ? 'now()' : $date,
+            $format === 'null' ? null : $format,
+            $locale === 'null' ? null : $locale,
+        ];
     }
 }
