ef71b1564b2865351dc3d3a1c085a471173d80a4
AnuzPandey <anuzbvbmaniac123@gmail.com>
2024-04-07 22:36:01 +0545

    :hammer: fix: blade directives signature.

--- src/Directives/NepaliDateDirective.php
+++ src/Directives/NepaliDateDirective.php
@@ -8,12 +8,14 @@ class NepaliDateDirective
 {
     public static function register(): void
     {
-        Blade::directive('nepaliDate', function ($date, $format, $locale) {
+        Blade::directive('nepaliDate', function ($date = null, $format = null, $locale = null) {
+            $date ??= now();
             return "<?php echo Anuzpandey\LaravelNepaliDate\LaravelNepaliDate::from($date)->toNepaliDate($format, $locale) ?>";
         });
 
-        Blade::directive('englishDate', function ($date, $locale) {
-            return "<?php echo Anuzpandey\LaravelNepaliDate\LaravelNepaliDate::from($date)->toEnglishDate($locale) ?>";
+        Blade::directive('englishDate', function ($date = null, $format = null, $locale = null) {
+            $date ??= now();
+            return "<?php echo Anuzpandey\LaravelNepaliDate\LaravelNepaliDate::from($date)->toEnglishDate($format, $locale) ?>";
         });
     }
 }
