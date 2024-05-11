388ed4702e010cd380c3af981ce2f398183d9566
AnuzPandey <anuzbvbmaniac123@gmail.com>
2024-05-12 01:21:11 +0545

    :zap: feat: add Nepali Date Data static property and Nepali Month Enum Helpers.

---
+++ src/Enums/NepaliMonth.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Anuzpandey\LaravelNepaliDate\Enums;
+
+enum NepaliMonth: int
+{
+    case BAISAKH = 1;
+    case JESTHA = 2;
+    case ASAR = 3;
+    case SHRAWAN = 4;
+    case BHADRA = 5;
+    case ASWIN = 6;
+    case KARTIK = 7;
+    case MANGSIR = 8;
+    case POUSH = 9;
+    case MAGH = 10;
+    case FALGUN = 11;
+    case CHAITRA = 12;
+}
